c22ef90ab65338fc3d64d3ae0b7c52b2314484e1
Litvinov Sergey <slitvinov@gmail.com>
2012-11-07 14:10:23 +0100

    Add a color gradient and surface tension forces to SPH. Modify pack_comm_hybrid() to make atom_vec_meso work with fix_deform.
    Former-commit-id: ee49615d5d9e8a1cbe5091422537ba6b3ba5f295 [formerly 45acd244f21df9a04a85b2d95e728ee6460d42e3]
    Former-commit-id: f66831cad6a50a75c918aebf7b20d9d6ef1204a5

--- src/USER-SPH/Install.sh
+++ src/USER-SPH/Install.sh
@@ -2,6 +2,7 @@
 
 if (test $1 = 1) then
 
+  cp -p pair_sph_colorgradient.cpp ..
   cp -p atom_vec_meso.cpp ..
   cp -p pair_sph_heatconduction.cpp ..
   cp -p pair_sph_idealgas.cpp ..
@@ -9,12 +10,15 @@ if (test $1 = 1) then
   cp -p pair_sph_rhosum.cpp ..
   cp -p pair_sph_taitwater.cpp ..
   cp -p pair_sph_taitwater_morris.cpp ..
+  cp -p pair_sph_surfacetension.cpp ..
   cp -p compute_meso_e_atom.cpp ..
   cp -p compute_meso_rho_atom.cpp ..
+  cp -p compute_meso_colorgradient_atom.cpp ..
   cp -p compute_meso_t_atom.cpp ..
   cp -p fix_meso.cpp ..
   cp -p fix_meso_stationary.cpp ..
 
+  cp -p pair_sph_colorgradient.h ..
   cp -p atom_vec_meso.h ..
   cp -p pair_sph_heatconduction.h ..
   cp -p pair_sph_idealgas.h ..
@@ -22,14 +26,16 @@ if (test $1 = 1) then
   cp -p pair_sph_rhosum.h ..
   cp -p pair_sph_taitwater.h ..
   cp -p pair_sph_taitwater_morris.h ..
+  cp -p pair_sph_surfacetension.h ..
   cp -p compute_meso_e_atom.h ..
   cp -p compute_meso_rho_atom.h ..
+  cp -p compute_meso_colorgradient_atom.h ..
   cp -p compute_meso_t_atom.h ..
   cp -p fix_meso.h ..
   cp -p fix_meso_stationary.h ..
 
 elif (test $1 = 0) then
-
+  rm -f ../pair_sph_colorgradient.cpp
   rm -f ../atom_vec_meso.cpp
   rm -f ../pair_sph_heatconduction.cpp
   rm -f ../pair_sph_idealgas.cpp
@@ -37,12 +43,15 @@ elif (test $1 = 0) then
   rm -f ../pair_sph_rhosum.cpp
   rm -f ../pair_sph_taitwater.cpp
   rm -f ../pair_sph_taitwater_morris.cpp
+  rm -f ../pair_sph_surfacetension.cpp
   rm -f ../compute_meso_e_atom.cpp
   rm -f ../compute_meso_rho_atom.cpp
+  rm -f ../compute_meso_colorgradient_atom.cpp
   rm -f ../compute_meso_t_atom.cpp
   rm -f ../fix_meso.cpp
   rm -f ../fix_meso_stationary.cpp
 
+  rm -f ../pair_sph_colorgradient.h
   rm -f ../atom_vec_meso.h
   rm -f ../pair_sph_heatconduction.h
   rm -f ../pair_sph_idealgas.h
@@ -50,8 +59,10 @@ elif (test $1 = 0) then
   rm -f ../pair_sph_rhosum.h
   rm -f ../pair_sph_taitwater.h
   rm -f ../pair_sph_taitwater_morris.h
+  rm -f ../pair_sph_surfacetension.h
   rm -f ../compute_meso_e_atom.h
   rm -f ../compute_meso_rho_atom.h
+  rm -f ../compute_meso_colorgradient_atom.h
   rm -f ../compute_meso_t_atom.h
   rm -f ../fix_meso.h
   rm -f ../fix_meso_stationary.h
