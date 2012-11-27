47e4dfb2d39a620d94f91b2904faa0459907d116
Litvinov Sergey <slitvinov@gmail.com>
2012-11-27 20:20:55 +0100

    Droplet
    Former-commit-id: f4c8d88a42ca8b154e2ae0a660bebf5343d4eb0c

---
+++ examples/USER/sph/droplet-grid/genpsf.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -e
+set -u
+
+input=data-wall/droplet.restart
+datafile=${input/.restart/.dat}
+output=${input/.restart/.psf}
+
+../../../../tools/restart2data ${input} ${datafile}
+
+tmpfile=$(mktemp /tmp/XXXXX)
+# generate psf file with vmd
+vmd -dispdev text -eofexit <<EOF
+package require topotools
+topo readlammpsdata  ${datafile} full
+animate write psf ${tmpfile}
+EOF
+
+sed 's/ NAMD EXT//g' ${tmpfile} > ${output}
