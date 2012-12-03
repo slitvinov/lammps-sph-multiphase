ccc2aa52b0411ce5bfa366da7cf10744a57f954c
Litvinov Sergey <slitvinov@gmail.com>
2012-12-03 18:07:11 +0100

    Update of config files
    Former-commit-id: 7dd0b040046ad6cbf95c64d5cf8b3c67c5d2a2e4

--- examples/USER/sph/bubble-in-superheated/run.sh
+++ examples/USER/sph/bubble-in-superheated/run.sh
@@ -2,5 +2,5 @@
 
 dname=data-wall
 mkdir -p ${dname}
-mpirun -np 1  ../../../../src/lmp_linux -in insert.lmp -var dname ${dname}
+mpirun -np 4  ../../../../src/lmp_linux -in insert.lmp -var dname ${dname}
 
