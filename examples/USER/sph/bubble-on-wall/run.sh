45f89f0b9192956c47ed6e9cb4253a4389a3d1ef
Litvinov Sergey <slitvinov@gmail.com>
2012-12-06 20:07:33 +0100

    Update
    Former-commit-id: 633e85e92b51277de8a9be6dc270afddc824994c [formerly 907651753b0ccf2f74c0994f697249b8f7f272e9]
    Former-commit-id: 0bc673f3db4885b389c2580e85fac402725e4d5b

--- examples/USER/sph/bubble-on-wall/run.sh
+++ examples/USER/sph/bubble-on-wall/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 gy=$1
-dname=data-wall-gy${gy}
+dname=data-wall-gy${gy}-part3
 mkdir -p ${dname}
-mpirun -np 8  ../../../../src/lmp_linux -in insert.lmp -var gy ${gy} -var dname ${dname}
+mpirun -np 1  ../../../../src/lmp_linux -in insert.lmp -var gy ${gy} -var dname ${dname}
 
--- examples/USER/sph/bubble-on-wall/stage.sh
+++ examples/USER/sph/bubble-on-wall/stage.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
-for g in 1.0 1.25 1.5 2.0 3.0; do
-    Np=$(awk '$3>1.0&&NR>1{print $2;exit}' data-wall-gy${g}/rg.dat)
+function getnp_depart() {
+    awk '$3>1.0&&NR>1{print $2;exit}' $1
+}
+
+function getg() {
+    echo  $1 | sed -e 's/.*gy//1' -e 's/-.*//1'
+}
+
+
+for d in $(ls -d data-wall-gy*); do
+    g=$(getg $d)
+    Np=$(getnp_depart $d/rg.dat)
     echo $g ${Np}
-done
+done | awk 'NF==2'
 
 
