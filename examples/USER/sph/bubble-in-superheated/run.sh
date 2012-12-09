9806183d1bb7a5fbdfbb9272793ad43b162c5c98
Litvinov Sergey <slitvinov@gmail.com>
2012-12-09 23:55:51 +0100

    Update
    Former-commit-id: 324b978eba7b8cc15df088945a0e9acd4a31713c [formerly 0d410f745563c26e65ef5ba7b87bdefb1a1fd403]
    Former-commit-id: 27b2eee652ba318564ffa2701f4c45e13369a585

--- examples/USER/sph/bubble-in-superheated/run.sh
+++ examples/USER/sph/bubble-in-superheated/run.sh
@@ -3,7 +3,7 @@
 nx=40
 ndim=3
 np=8
-dname=data-nx${nx}-ndim${ndim}-np${np}b
+dname=data-nx${nx}-ndim${ndim}-np${np}a
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
