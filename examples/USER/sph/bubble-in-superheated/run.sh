6833ef120c0c58fa5050114288a93b23faebb3bd
Litvinov Sergey <slitvinov@gmail.com>
2012-12-09 17:45:44 +0100

    Update of bubble-in-superheated
    Former-commit-id: 3fe90a17e7cc22213b97409efd738632154e5d0d [formerly 8dd857cafe18f59be5743644a69443c818f4434b]
    Former-commit-id: fd0f4fe9b0ddfdf738a722c3d6ccf59a6ed0d340

--- examples/USER/sph/bubble-in-superheated/run.sh
+++ examples/USER/sph/bubble-in-superheated/run.sh
@@ -2,5 +2,5 @@
 
 dname=data-wall3
 mkdir -p ${dname}
-mpirun -np 1  ../../../../src/lmp_linux -in insert.lmp -var dname ${dname}
+mpirun -np 8  ../../../../src/lmp_linux -in insert.lmp -var dname ${dname}
 
