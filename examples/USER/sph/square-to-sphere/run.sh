02349e877b035639062ac946adceacbebcc39775
Litvinov Sergey <slitvinov@gmail.com>
2012-12-10 17:39:00 +0100

    Update square-to-sphere and bubble-simple
    Former-commit-id: 0788ac12008d47f3a34f2bce6b57faa1ee4f6e4a

--- examples/USER/sph/square-to-sphere/run.sh
+++ examples/USER/sph/square-to-sphere/run.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-dname=data-wall
+nx=40
+ndim=3
+np=8
+dname=data-nx${nx}-ndim${ndim}-np${np}a
 mkdir -p ${dname}
-mpirun -np 4  ../../../../src/lmp_linux -in insert.lmp -var dname ${dname}
-
+mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
