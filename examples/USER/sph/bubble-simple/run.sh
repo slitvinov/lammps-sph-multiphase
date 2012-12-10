0e40c1ca9a1e203cac617951e5b9cdc1b5df6c85
Litvinov Sergey <slitvinov@gmail.com>
2012-12-10 23:18:26 +0100

    Running
    Former-commit-id: ea0a6dcee702e463dc4dd26c326c04be8d186667

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-nx=80
+nx=40
 ndim=3
 np=8
-dname=data-nx${nx}-ndim${ndim}-np${np}-perpcg-withst
+dname=data-nx${nx}-ndim${ndim}-np${np}-random
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
