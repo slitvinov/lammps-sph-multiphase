7478da0fb5aaa134883112ee631c3f0d0ee75ae9
Litvinov Sergey <slitvinov@gmail.com>
2012-12-11 20:44:10 +0100

    Fix bug in phase change
    Former-commit-id: 74cd0c31ac4180825814f7084e85a66098b685c8

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-nx=40
+nx=$1
 ndim=3
 np=8
-dname=data-nx${nx}-ndim${ndim}-np${np}-random
+dname=data-nx${nx}-ndim${ndim}-np${np}-wall
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
