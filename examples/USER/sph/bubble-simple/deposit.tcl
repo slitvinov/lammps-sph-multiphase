35f20d737d2efd0220cd88d88eccfe1d96988480
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 11:32:07 +0100

    Testing 3D bubble
    Former-commit-id: 0af1c1aeb4ec84b3b8b012e9790f7a685cd05611 [formerly 65eb43e25f7389c84c5b5714f151830dee2f2c15]
    Former-commit-id: 438ede279860b5781def2d1bbde681e318e8b472

--- examples/USER/sph/bubble-simple/deposit.tcl
+++ examples/USER/sph/bubble-simple/deposit.tcl
@@ -10,8 +10,8 @@ pbc set {1.0 1.0 1.0} -all
 set sel [atomselect top all]
 $sel set radius 0.03
 
-mol modselect 0 0 (all) and user > 0 and name B
-mol modstyle 0 0 VDW 0.600000 12.000000
+mol modselect 0 0 (all) and user > 0 and z>0.42 and z<0.58
+#mol modstyle 0 0 VDW 0.600000 12.000000
 
 pbc box
 
--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -3,7 +3,7 @@
 nx=$1
 ndim=3
 np=1
-dname=data-nx${nx}-ndim${ndim}-np${np}-withTc
+dname=data-nx${nx}-ndim${ndim}-np${np}-alpha5
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
