e11962cdb0e8544eb4304067c292237f04b63417
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 01:48:10 +0100

    Add half space case, fix a bug in bubble-in-superheated
    Former-commit-id: 5a39eff817a6e44b220b12f881ae9c7ad4403795 [formerly a1e4fca86f8ae8c365121e4a2877f4fa216f80b3]
    Former-commit-id: bf043c157429eb5cc2eb4787ce7a4afff95ebaf4

--- examples/USER/sph/half-phase/deposit.tcl
+++ examples/USER/sph/half-phase/deposit.tcl
@@ -1,5 +1,6 @@
 # read lammps files with "fix deposit"
 log vmd.tcl
+user add key q exit
 
 # read variable number of atoms
 topo readvarxyz [lindex $argv 0]/data.xyz
@@ -9,7 +10,7 @@ pbc set {1.0 1.0 1.0} -all
 set sel [atomselect top all]
 $sel set radius 0.03
 
-#mol modselect 0 0 (all) and user > 0 and name B
+mol modselect 0 0 (all) and user > 0 and name B
 mol modstyle 0 0 VDW 0.600000 12.000000
 
 pbc box
