bf043c157429eb5cc2eb4787ce7a4afff95ebaf4
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 01:48:10 +0100

    Add half space case, fix a bug in bubble-in-superheated
    Former-commit-id: a1e4fca86f8ae8c365121e4a2877f4fa216f80b3

--- examples/USER/sph/half-phase/run.sh
+++ examples/USER/sph/half-phase/run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-nx=20
+nx=$1
 ndim=2
 np=1
-dname=data-nx${nx}-ndim${ndim}-np${np}a
+dname=data-nx${nx}-ndim${ndim}-np${np}-withTc
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
