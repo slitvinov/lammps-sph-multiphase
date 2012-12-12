dfb4cda08980aa290a71bca711ee489620db77e8
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 23:16:56 +0100

    Add half a domain to post.sh
    Former-commit-id: 51fb61db7392affd4acbf04ac9992d4dfa22388f [formerly 3d7e27f32b9c616da8646aa824cfac7725d3f76d]
    Former-commit-id: 89c6b69a43b9eaaf84a522cb26ccb1c8da6a6055

--- examples/USER/sph/bubble-simple/post.sh
+++ examples/USER/sph/bubble-simple/post.sh
@@ -2,5 +2,6 @@
 
 ../script/lammps2punto.sh $1/dump* | awk 'NF{print $3, $4, $5, $6, $7, $9, $NF, $2} !NF' > $1/temp.dat
 ../script/lammps2punto.sh $1/dump* | awk 'NF&&$2==2{print $3, $4, $5, $6, $7, $9, $NF, $2} !NF' > $1/bubble.dat
+../script/lammps2punto.sh $1/dump* | awk 'NF&&$3<0.5{print $3, $4, $5, $6, $7, $9, $NF, $2} !NF' > $1/half.dat
 
 awk 'f{print $1; f=0; nextfile} /ITEM: NUMBER OF ATOMS/{f=1}' $1/dump* > $1/n.dat
