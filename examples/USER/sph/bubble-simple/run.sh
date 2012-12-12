8f448c90065e6b9fe95402ab4b4ee00d1dc2d057
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 20:42:20 +0100

    Update of surface tension formulas
    Former-commit-id: 2fc2766e490df43f146bdde7ccc1b808053cc435

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -4,11 +4,13 @@ nx=40
 np=8
 ndim=3
 D_heat_g=0.0005
-alpha=2.0
+alpha=0.50
+sph_c_g=10.0
 
-dname=data-nx${nx}-ndim${ndim}-np${np}-D_heat_g${D_heat_g}-alphs${alpha}
+dname=data-nx${nx}-ndim${ndim}-np${np}-D_heat_g${D_heat_g}-alpha${alpha}-sph_c_g${sph_c_g}
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp \
-    -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
+    -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
+    -var sph_c_g ${sph_c_g} -var dname ${dname}
 
