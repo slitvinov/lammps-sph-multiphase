202982f647cf38d8d589b77bb36493cbc8c4915b
Litvinov Sergey <slitvinov@gmail.com>
2012-12-12 12:01:04 +0100

    Merge with laptop
    Former-commit-id: 0b31bfd688b9a363e396f3f341e469f116023bb7 [formerly 510e09905c7fc9f20b22320039aa998995d9769a]
    Former-commit-id: 1d5814bad6d09c265945b6dd656163bad5a61eb5

--- examples/USER/sph/droplet/post.tcl
+++
@@ -1,8 +0,0 @@
-log vmd.tcl
-
-mol modstyle 0 0 Points 21.0
-set sel_solvent [atomselect top "resname 1"]
-$sel_solvent set name water
-
-set sel_solvent [atomselect top "resname 2"]
-$sel_solvent set name droplet
