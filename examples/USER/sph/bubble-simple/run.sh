31391e250b863aabd1d5350f9157b1bd392a6910
Litvinov Sergey <slitvinov@gmail.com>
2012-12-15 00:12:34 +0100

    Update
    Former-commit-id: 2498610dd04a2ce16daf059d9cd0a456118a2f94 [formerly ec772b516ccf9468781e6f7418f6146b8400fc3a]
    Former-commit-id: 2925cac51a3d915a8d873f65e65e01842dbd4a35

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
-nx=40
+Lx=1.0
+nx=20
 np=8
 ndim=3
-D_heat_g=0.05
-cp=0.50
+D_heat_g=0.005
+cp=0.2
 alpha=0.5
 sph_c_g=10.0
-prob=1.0
-time_k=4.00
+prob=0.1
+time_k=2.00
 
-dname=data-nx${nx}-ndim${ndim}-np${np}-D_heat_g${D_heat_g}-alpha${alpha}-sph_c_g${sph_c_g}-cp${cp}-prob${prob}-time_k${time_k}
+dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-np${np}-D_heat_g${D_heat_g}-alpha${alpha}-sph_c_g${sph_c_g}-cp${cp}-prob${prob}-time_k${time_k}dist
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp \
     -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
     -var sph_c_g ${sph_c_g} -var cp ${cp} -var prob ${prob} -var time_k ${time_k} \
+    -var Lx ${Lx} \
     -var dname ${dname}
 
