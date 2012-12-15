41e19de88e6dbe72206bafe6b776840618117e1c
Litvinov Sergey <slitvinov@gmail.com>
2012-12-16 00:03:49 +0100

    Testing rho_b/rho_l = 10
    Former-commit-id: 1d7141808b58d8f1f668af0802d4b1ae93e453c8

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -9,14 +9,15 @@ D_heat_g=0.05
 cp=0.1
 alpha=0.5
 prob=0.1
+sph_rho_d=0.1
 time_k=1.00
 cv_g=2.0
 
-dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}-cp${cp}-prob${prob}-time_k${time_k}-cv_g${cv_g}
+dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}-cp${cp}-prob${prob}-time_k${time_k}-cv_g${cv_g}-sph_rho_d${sph_rho_d}
 rm -rf ${dname}
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp \
     -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
     -var cp ${cp} -var prob ${prob} -var time_k ${time_k} \
-    -var Lx ${Lx} -var cv_g ${cv_g} \
+    -var Lx ${Lx} -var cv_g ${cv_g} -var sph_rho_d ${sph_rho_d} \
     -var dname ${dname}
