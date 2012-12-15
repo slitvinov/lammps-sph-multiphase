32396a6336788ad06938497bd5664017ce2180cd
Litvinov Sergey <slitvinov@gmail.com>
2012-12-15 18:20:15 +0100

    For smuc
    Former-commit-id: e866ea1b756e3917107a46b16cf3f811f0c80a1f

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-Lx=1.5
-nx=80
-np=1
-ndim=2
-D_heat_g=0.05
-cp=0.05
-alpha=0.01
+Lx=1.50
+nx=40
+np=8
+ndim=3
+D_heat_g=0.1
+cp=1.00
+alpha=0.125
 sph_c_g=10.0
 prob=1.0
-time_k=0.25
+time_k=1.00
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-np${np}-D_heat_g${D_heat_g}-alpha${alpha}-sph_c_g${sph_c_g}-cp${cp}-prob${prob}-time_k${time_k}grad
 rm -rf ${dname}
