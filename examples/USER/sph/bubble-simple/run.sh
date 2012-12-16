8401f3ed29c1453a2b162eecc2baaa0d865b093f
Litvinov Sergey <slitvinov@gmail.com>
2012-12-16 21:56:32 +0100

    With "naive" energy distribution
    Former-commit-id: 2ebd52deb492488391926f81464a12f6519d175a [formerly 5a58f79aedcff71a1463e56b44aa31f5a8134a27]
    Former-commit-id: 1d3583db6376b086989054ae348544b7464b2dc7

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,25 +1,30 @@
 #! /bin/bash
 
+set -e
+set -u
+dTarray[160]=0.0141
+dTarray[80]=0.0200
+dTarray[40]=0.0283
+dTarray[20]=0.0400
 Lx=1.0
 nx=40
 ndim=3
 np=8
-D_heat_g=0.05
-dT=0.0283
-cp=0.4
+D_heat_g=0.04
+dT=0.01
+cp=1.0
 alpha=0.25
-#dprob=5.5e-4
 dprob=0.1
 sph_rho_d=1.0
 time_k=1.00
-cv_g=2.0
+cv_g=4.0
 # parameters for kana
 lmp=../../../../src/lmp_linux
 mpirun=mpirun
 proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}\
--cp${cp}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_d${sph_rho_d}-dT${dT}conv
+-cp${cp}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_d${sph_rho_d}-dT${dT}heat
 
 rm -rf ${dname}
 mkdir -p ${dname}
