4b2d752fe9e7cd207431c508fc3a0692fdf22ec6
Litvinov Sergey <slitvinov@gmail.com>
2012-12-16 19:43:10 +0100

    Converges for 2D
    Former-commit-id: 99b975c9403139e45038c78706205cb69e5d2394

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
 Lx=1.0
-nx=20
+nx=160
 ndim=2
-np=1
+np=8
 D_heat_g=0.05
-cp=0.1
-alpha=0.5
-prob=0.01
+dT=0.014
+cp=0.4
+alpha=0.25
+#dprob=5.5e-4
+dprob=0.1
 sph_rho_d=1.0
 time_k=1.00
 cv_g=2.0
@@ -17,12 +19,12 @@ mpirun=mpirun
 proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}\
--cp${cp}-prob${prob}-time_k${time_k}-cv_g${cv_g}-sph_rho_d${sph_rho_d}
+-cp${cp}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_d${sph_rho_d}-dT${dT}conv
 
 rm -rf ${dname}
 mkdir -p ${dname}
 ${mpirun} ${proc} ${lmp} -in insert.lmp \
     -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
-    -var cp ${cp} -var prob ${prob} -var time_k ${time_k} \
-    -var Lx ${Lx} -var cv_g ${cv_g} -var sph_rho_d ${sph_rho_d} \
+    -var cp ${cp} -var dprob ${dprob} -var time_k ${time_k} \
+    -var Lx ${Lx} -var cv_g ${cv_g} -var sph_rho_d ${sph_rho_d} -var dT ${dT} \
     -var dname ${dname}
