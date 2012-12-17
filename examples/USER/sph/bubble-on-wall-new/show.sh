fb62c4d972605413d21d7e32dd0606e6eab98d58
Litvinov Sergey <slitvinov@gmail.com>
2012-12-17 21:13:38 +0100

    Running a departure simulation
    Former-commit-id: 192711d7edc4dfc17cdac53a352a99dc1b7be5a6 [formerly 895d9973f293a017c5e81334fc23e2adff201dd0]
    Former-commit-id: 3d10d1270a3fb78931b461e390bb0acadc63afb6

---
+++ examples/USER/sph/bubble-on-wall-new/show.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+Tc=0.85
+B="0:0:0:1.0:1.5:1.0"
+if [ $# -eq 2 ]; then
+    n=$2
+else
+    n=1
+fi
+
+dn=$(ls -1td data* | awk -v n=$n 'NR==n' )
+bash post.sh ${dn}
+
+if [ $1 == "a" ]; then
+    punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 ${dn}/temp.dat
+elif [ $1 == "b" ]; then
+    punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 ${dn}/bubble.dat
+elif [ $1 == "s" ]; then
+    punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 ${dn}/slice.x.dat
+elif [ $1 == "c" ]; then
+    punto -B ${B} -c -z 1:2:3:8 -s 4 ${dn}/temp.dat
+fi
+
+echo ${dn}
