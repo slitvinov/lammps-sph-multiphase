b88bc7b2c8001173b38dd683d8c96915fc0c41fe
Litvinov Sergey <slitvinov@gmail.com>
2012-12-18 19:43:31 +0100

    Supermuc
    Former-commit-id: c0d97019ab0a6f9d3df4e93e8b732ade4fcde6b3 [formerly a6d2db9cd8baf954bf1303226494cabc3b5405e9]
    Former-commit-id: aa295f37809e6d13e8a578c92ace8933b8a54935

--- examples/USER/sph/bubble-on-wall-new/run.sh
+++ examples/USER/sph/bubble-on-wall-new/run.sh
@@ -2,14 +2,15 @@
 
 set -e
 set -u
-Lx=1.5
+Lx=1.0
 nx=$1
 ndim=2
 np=8
-D_heat_g=0.02
+D_heat_g=0.04
 dT=0.01
+Tc=0.85
 Hwv=1.0
-alpha=1.00
+alpha=0.50
 dprob=0.01
 sph_rho_g=10.0
 time_k=1.0
@@ -22,7 +23,7 @@ proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}\
 -Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_g${sph_rho_g}-dT${dT}\
--gy${gy}
+-gy${gy}-Tc${Tc}
 
 rm -rf ${dname}
 mkdir -p ${dname}
@@ -30,5 +31,5 @@ ${mpirun} ${proc} ${lmp} -in insert.lmp \
     -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
     -var Hwv ${Hwv} -var dprob ${dprob} -var time_k ${time_k} \
     -var Lx ${Lx} -var cv_g ${cv_g} -var sph_rho_g ${sph_rho_g} -var dT ${dT} \
-    -var gy ${gy} \
+    -var gy ${gy} -var Tc ${Tc} \
     -var dname ${dname}
