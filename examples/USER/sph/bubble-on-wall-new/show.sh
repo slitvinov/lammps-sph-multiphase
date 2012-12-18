842a7fc121d1de7f8e744f9ec3d0d9bf00be308a
Litvinov Sergey <slitvinov@gmail.com>
2012-12-18 17:57:23 +0100

    For smuc
    Former-commit-id: 4a8d8fa8423ec9cb082adebbac95a0287cae21dc [formerly 99d1d5e61200d2b8be001cb7290b581c8fa4951b]
    Former-commit-id: 5ee1d4378a051666638ae751fe3f4e05317b642e

--- examples/USER/sph/bubble-on-wall-new/show.sh
+++ examples/USER/sph/bubble-on-wall-new/show.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 Tc=0.85
-B="0:0:0:1.0:1.5:1.0"
+B="0:0:0:1.5:1.5:1.5"
 if [ $# -eq 2 ]; then
     n=$2
 else
@@ -12,7 +12,7 @@ dn=$(ls -1td data* | awk -v n=$n 'NR==n' )
 bash post.sh ${dn}
 
 if [ $1 == "a" ]; then
-    punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 ${dn}/temp.dat
+    punto -B ${B} -G ${Tc}:1.00 -s 1 -z 1:2:3:7 ${dn}/temp.dat
 elif [ $1 == "b" ]; then
     punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 ${dn}/bubble.dat
 elif [ $1 == "s" ]; then
