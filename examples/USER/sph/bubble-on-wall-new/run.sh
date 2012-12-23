5fe8720232181ed4fc450d5da8a2a62566704f41
Litvinov Sergey <slitvinov@gmail.com>
2012-12-23 14:19:32 +0100

    Add min.sh, update of the configuration files
    Former-commit-id: a735a075e6c793432b5244cac3b418cff1588272

--- examples/USER/sph/bubble-on-wall-new/run.sh
+++ examples/USER/sph/bubble-on-wall-new/run.sh
@@ -5,14 +5,14 @@ set -u
 Lx=1.0
 nx=$1
 ndim=2
-np=8
+np=1
 D_heat_g=0.04
 dT=0.01
 Tc=0.85
 Hwv=4.0
-alpha=0.125
+alpha=0.25
 dprob=0.1
-sph_rho_g=10.0
+sph_rho_g=1.0
 time_k=1.00
 cv_g=4.0
 gy=$2
@@ -23,7 +23,7 @@ proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}\
 -Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_g${sph_rho_g}-dT${dT}\
--gy${gy}-Tc${Tc}dr
+-gy${gy}-Tc${Tc}n
 
 rm -rf ${dname}
 mkdir -p ${dname}
