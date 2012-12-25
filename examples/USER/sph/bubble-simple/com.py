9bd408adc132aaa6c112b8c35fd70ebcf2b4f134
Litvinov Sergey <slitvinov@gmail.com>
2012-12-25 22:58:46 +0100

    Add a template for a special heat conduction equation
    Former-commit-id: fe23ecf2a34194876d725ee035df941b49fdbfe5

---
+++ examples/USER/sph/bubble-simple/com.py
@@ -0,0 +1,57 @@
+# For emacs set
+# (setenv "PYTHONPATH" "/scratch/work/Pizza.py/src/")
+# (setenv "PYTHONPATH" "/home/vital303/work/Pizza.py/src/")
+
+import os.path
+import glob
+import dump
+import numpy as np
+import math
+
+os.system("./aux.sh")
+dname = "last1"
+d = dump.dump(os.path.join(dname, "dump*"))
+
+d.tselect.all()
+# list of times
+t = d.time()
+
+def interp(xi, x, y, h):
+    def kernel(r):
+        return math.exp(-r*r/ (h*h) )
+        #return (math.fabs(r)<h)+0.0
+    yc = np.zeros(np.size(xi))
+    wc = np.zeros(np.size(xi))
+    for i, xc in enumerate(xi):
+        for xp, yp in zip(x, y):
+            w = kernel(xp-xc)
+            yc[i] += w*yp
+            wc[i] += w
+    yc[wc>0] = yc[wc>0]/wc[wc>0]
+    return yc
+
+d.aselect.test("$type == 2")
+for tc in t:
+    d.aselect.test("$type == 2")
+    xd = d.vecs(tc, "x")
+    xcm = np.mean(xd)
+    yd = d.vecs(tc, "y")
+    ycm = np.mean(yd)
+    d.aselect.all()
+    x = d.vecs(tc, "x")
+    y = d.vecs(tc, "y")
+    r = np.sqrt(np.square(x-xcm) + np.square(y-ycm))
+    temperature = d.vecs(tc, "c_it_atom")
+    ri = np.linspace(min(r), max(r), 100)
+    #data = [r, temperature]
+    #Z2 = ndimage.gaussian_filter(data, 0.2, mode='constant');
+    temperature_smoothed = interp(ri, r, temperature, 0.01)
+    np.savetxt(os.path.join(dname, "com.int.%i" % tc),
+               np.column_stack( (ri, temperature_smoothed) ))
+    np.savetxt(os.path.join(dname, "com.dat.%i" % tc),
+               np.column_stack( (r, temperature) ))
+    
+
+
+
+
--- src/USER-SPH/Install.sh
+++ src/USER-SPH/Install.sh
@@ -7,6 +7,7 @@ if (test $1 = 1) then
   cp -p pair_sph_colorgradient.cpp ..
   cp -p atom_vec_meso.cpp ..
   cp -p pair_sph_heatconduction.cpp ..
+  cp -p pair_sph_heatconduction_phasechange.cpp ..
   cp -p pair_sph_idealgas.cpp ..
   cp -p pair_sph_lj.cpp ..
   cp -p pair_sph_rhosum.cpp ..
@@ -30,6 +31,7 @@ if (test $1 = 1) then
   cp -p pair_sph_colorgradient.h ..
   cp -p atom_vec_meso.h ..
   cp -p pair_sph_heatconduction.h ..
+  cp -p pair_sph_heatconduction_phasechange.h ..
   cp -p pair_sph_idealgas.h ..
   cp -p pair_sph_lj.h ..
   cp -p pair_sph_rhosum.h ..
@@ -54,6 +56,7 @@ elif (test $1 = 0) then
   rm -f ../pair_sph_colorgradient.cpp
   rm -f ../atom_vec_meso.cpp
   rm -f ../pair_sph_heatconduction.cpp
+  rm -f ../pair_sph_heatconduction_phasechange.cpp
   rm -f ../pair_sph_idealgas.cpp
   rm -f ../pair_sph_lj.cpp
   rm -f ../pair_sph_rhosum.cpp
@@ -77,6 +80,7 @@ elif (test $1 = 0) then
   rm -f ../pair_sph_colorgradient.h
   rm -f ../atom_vec_meso.h
   rm -f ../pair_sph_heatconduction.h
+  rm -f ../pair_sph_heatconduction_phasechange.h
   rm -f ../pair_sph_idealgas.h
   rm -f ../pair_sph_lj.h
   rm -f ../pair_sph_rhosum.h
