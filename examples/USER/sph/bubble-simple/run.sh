352878f26a9cec2e3421f9b4217209e3f748e0de
Litvinov Sergey <slitvinov@gmail.com>
2012-12-26 22:02:40 +0100

    For smuc
    Former-commit-id: 77ff7cf40ce50368e2e2bd8cf81357c80780fc1d [formerly 0758b88da52a858f6c13faa712bdace8a34c37b6]
    Former-commit-id: 8b08e6ec815fcd9b9095b3ad196c26df6aa85c21

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -3,18 +3,19 @@
 set -e
 set -u
 Lx=1.0
-nx=80
+nx=60
 ndim=3
 np=8
-D_heat_d=0.5
-D_heat_g=0.5
+D_heat_d=1.0
+D_heat_g=1.0
+sph_eta_d=0.69395
 cv_d=1.0
-cv_g=1.0
+cv_g=3.0
 dT=0.0
-Hwv=5.0
-alpha=100
+Hwv=10.0
+alpha=200
 dprob=0.01
-sph_rho_d=1
+sph_rho_d=1.0
 time_k=1.0
 # parameters for kana
 lmp=../../../../src/lmp_linux
@@ -23,7 +24,7 @@ proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_d${D_heat_d}-alpha${alpha}\
 -Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_d${cv_d}-sph_rho_d${sph_rho_d}-dT${dT}\
--cv_g${cv_g}-D_heat_g${D_heat_g}
+-cv_g${cv_g}-D_heat_g${D_heat_g}-sph_eta_d${sph_eta_d}
 
 rm -rf ${dname}
 mkdir -p ${dname}
@@ -32,4 +33,4 @@ ${mpirun} ${proc} ${lmp} -in insert.lmp \
     -var Hwv ${Hwv} -var dprob ${dprob} -var time_k ${time_k} \
     -var Lx ${Lx} -var cv_d ${cv_d} -var sph_rho_d ${sph_rho_d} -var dT ${dT} \
     -var cv_g ${cv_g} -var D_heat_g ${D_heat_g} \
-    -var dname ${dname}
+    -var dname ${dname} -var sph_eta_d ${sph_eta_d}
