09f76647d85ecda6981f4e8b006b898890f9549a
Litvinov Sergey <slitvinov@gmail.com>
2012-12-27 22:15:46 +0100

    For smuc
    Former-commit-id: f54a3ef9f245b9dbf4f1b83571db8e84634c53f2 [formerly 2c1553807a0d7e8a3a09e8b7a27e3b4b76880022]
    Former-commit-id: de3b4197fc0d49e05b23059df004170d09fd45a6

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -3,16 +3,16 @@
 set -e
 set -u
 Lx=1.0
-nx=30
-ndim=3
+nx=60
+ndim=2
 np=8
 D_heat_d=3.0
-D_heat_g=0.1
+D_heat_g=1.0
 sph_eta_d=0.69395
 cv_d=1.0
-cv_g=1.0
-dT=0.1
-Hwv=10.0
+cv_g=4.0
+dT=0.0
+Hwv=100.0
 alpha=100
 dprob=0.01
 sph_rho_d=0.1
