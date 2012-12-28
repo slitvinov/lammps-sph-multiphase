aec438a0fe591e6d7a41c7877b2e94a7ce562a90
Litvinov Sergey <slitvinov@gmail.com>
2012-12-29 00:18:11 +0100

    Testing simple case
    Former-commit-id: 1755e59392610bf1c40f498a862926721dce94fe

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -3,19 +3,19 @@
 set -e
 set -u
 Lx=1.0
-nx=60
+nx=20
 ndim=2
 np=8
-D_heat_d=3.0
+D_heat_d=1.0
 D_heat_g=1.0
 sph_eta_d=0.69395
 cv_d=1.0
 cv_g=4.0
-dT=0.0
-Hwv=100.0
+dT=0.1
+Hwv=1.0
 alpha=100
-dprob=0.01
-sph_rho_d=0.1
+dprob=0.1
+sph_rho_d=0.01
 time_k=1.0
 # parameters for kana
 lmp=../../../../src/lmp_linux
@@ -24,7 +24,7 @@ proc="-np ${np}"
 
 dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_d${D_heat_d}-alpha${alpha}\
 -Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_d${cv_d}-sph_rho_d${sph_rho_d}-dT${dT}\
--cv_g${cv_g}-D_heat_g${D_heat_g}-sph_eta_d${sph_eta_d}
+-cv_g${cv_g}-D_heat_g${D_heat_g}-sph_eta_d${sph_eta_d}bug
 
 rm -rf ${dname}
 mkdir -p ${dname}
