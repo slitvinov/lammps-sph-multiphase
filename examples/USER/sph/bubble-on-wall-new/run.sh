2d7618a161513dd8efab01d3a4dc91775a8de69d
Litvinov Sergey <slitvinov@gmail.com>
2012-12-29 20:33:43 +0100

    Testing rising bubble
    Former-commit-id: 9e57c2074f9f20f006828463fb6ab42c7ce12b9d

--- examples/USER/sph/bubble-on-wall-new/run.sh
+++ examples/USER/sph/bubble-on-wall-new/run.sh
@@ -2,34 +2,36 @@
 
 set -e
 set -u
-Lx=2.0
-nx=$1
+Lx=1.0
+nx=40
 ndim=2
 np=1
-D_heat_g=0.04
-dT=0.01
-Tc=0.95
-Hwv=32.0
-alpha=0.25
-dprob=0.1
-sph_rho_g=10.0
-time_k=1.00
-cv_g=4.0
-gy=$2
+D_heat_d=0.6
+D_heat_g=0.1
+sph_eta_d=0.69395
+cv_d=1.0
+cv_g=0.04
+dT=0.1
+Hwv=8.0
+alpha=100
+dprob=0.01
+sph_rho_d=0.01
+time_k=1.0
+gy=1e5
 # parameters for kana
 lmp=../../../../src/lmp_linux
 mpirun=mpirun
 proc="-np ${np}"
 
-dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_g${D_heat_g}-alpha${alpha}\
--Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_g${cv_g}-sph_rho_g${sph_rho_g}-dT${dT}\
--gy${gy}-Tc${Tc}g
+dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_d${D_heat_d}-alpha${alpha}\
+-Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_d${cv_d}-sph_rho_d${sph_rho_d}-dT${dT}\
+-cv_g${cv_g}-D_heat_g${D_heat_g}-sph_eta_d${sph_eta_d}-gy${gy}
 
 rm -rf ${dname}
 mkdir -p ${dname}
-${mpirun} ${proc} ${lmp} -in insert.lmp \
-    -var alpha ${alpha} -var D_heat_g ${D_heat_g} -var ndim ${ndim} -var nx ${nx} \
+${lmp} -in insert.lmp \
+    -var alpha ${alpha} -var D_heat_d ${D_heat_d} -var ndim ${ndim} -var nx ${nx} \
     -var Hwv ${Hwv} -var dprob ${dprob} -var time_k ${time_k} \
-    -var Lx ${Lx} -var cv_g ${cv_g} -var sph_rho_g ${sph_rho_g} -var dT ${dT} \
-    -var gy ${gy} -var Tc ${Tc} \
-    -var dname ${dname}
+    -var Lx ${Lx} -var cv_d ${cv_d} -var sph_rho_d ${sph_rho_d} -var dT ${dT} \
+    -var cv_g ${cv_g} -var D_heat_g ${D_heat_g} \
+    -var dname ${dname} -var sph_eta_d ${sph_eta_d} -var gy ${gy}
--- examples/USER/sph/bubble-on-wall-new/show.sh
+++ examples/USER/sph/bubble-on-wall-new/show.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-Tc=0.95
+Tc=0.0
 #B="0:0:0:.75:3:2"
-B="0:0:0:1.0:2.0:2.0"
+B="0:0:0:1.0:1.0:1.0"
 if [ $# -eq 2 ]; then
     n=$2
 else
@@ -23,7 +23,7 @@ elif [ $1 == "s" ]; then
     punto -B ${B} -G ${Tc}:1.00 -z 1:2:3:7 slice.x.dat
 elif [ $1 == "c" ]; then
     cd ${dn}
-    punto -B ${B} -c -z 1:2:3:8 -s 4 temp.dat
+    punto -B ${B} -c -z 1:2:3:8 -s 2 temp.dat
 fi
 
 echo ${dn}
