783ed6d7db5e3417b9f8e2eb51f8010e64100cac
Litvinov Sergey <slitvinov@gmail.com>
2013-01-18 12:25:06 +0100

    Change c_d to c_v
    Former-commit-id: eaf3f9110b789c8ffd28580fc38b00d290882c3f

--- examples/USER/sph/bubble-simple/run.sh
+++ examples/USER/sph/bubble-simple/run.sh
@@ -4,33 +4,33 @@ set -e
 set -u
 Lx=1.0
 nx=40
-ndim=3
+ndim=2
 np=8
-D_heat_d=0.6
-D_heat_g=0.2
-sph_eta_d=0.69395
-cv_d=0.4
-cv_g=0.04
-dT=5.0
+D_heat_v=0.6
+D_heat_l=0.2
+sph_eta_v=0.69395
+cv_v=0.04
+cv_l=0.04
+dT=0.0
 Hwv=4.0
-alpha=100
-dprob=0.1
-sph_rho_d=0.01
+alpha=300
+dprob=0.01
+sph_rho_v=0.01
 time_k=1.0
 # parameters for kana
 lmp=../../../../src/lmp_linux
 mpirun=mpirun
 proc="-np ${np}"
 
-dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_d${D_heat_d}-alpha${alpha}\
--Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_d${cv_d}-sph_rho_d${sph_rho_d}-dT${dT}\
--cv_g${cv_g}-D_heat_g${D_heat_g}-sph_eta_d${sph_eta_d}m
+dname=data-nx${nx}-ndim${ndim}-Lx${Lx}-D_heat_v${D_heat_v}-alpha${alpha}\
+-Hwv${Hwv}-dprob${dprob}-time_k${time_k}-cv_v${cv_v}-sph_rho_v${sph_rho_v}-dT${dT}\
+-cv_l${cv_l}-D_heat_l${D_heat_l}-sph_eta_v${sph_eta_v}m
 
 rm -rf ${dname}
 mkdir -p ${dname}
 ${mpirun} ${proc} ${lmp} -in insert.lmp \
-    -var alpha ${alpha} -var D_heat_d ${D_heat_d} -var ndim ${ndim} -var nx ${nx} \
+    -var alpha ${alpha} -var D_heat_v ${D_heat_v} -var ndim ${ndim} -var nx ${nx} \
     -var Hwv ${Hwv} -var dprob ${dprob} -var time_k ${time_k} \
-    -var Lx ${Lx} -var cv_d ${cv_d} -var sph_rho_d ${sph_rho_d} -var dT ${dT} \
-    -var cv_g ${cv_g} -var D_heat_g ${D_heat_g} \
-    -var dname ${dname} -var sph_eta_d ${sph_eta_d}
+    -var Lx ${Lx} -var cv_v ${cv_v} -var sph_rho_v ${sph_rho_v} -var dT ${dT} \
+    -var cv_l ${cv_l} -var D_heat_l ${D_heat_l} \
+    -var dname ${dname} -var sph_eta_v ${sph_eta_v}
