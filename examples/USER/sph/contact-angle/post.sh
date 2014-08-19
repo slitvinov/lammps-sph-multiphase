1ece757b669bf6ee4898a81cae5d648b1aa596a0
Litvinov Sergey <slitvinov@gmail.com>
2014-08-19 19:33:31 +0200

    Update examples
    Former-commit-id: 2044e8b3152977fbb51d15f9a961779e1ed2efa7 [formerly dd23bdfa2d612ad765f7ea59c83a5a6a30f6230c]
    Former-commit-id: 1f6cc5d316bca154ea3aaa5d641d11597f80ca3e

--- examples/USER/sph/contact-angle/post.sh
+++ examples/USER/sph/contact-angle/post.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # x, y, fx, fy, colorgradient_peratom
-../script/lammps2punto.sh data-wall/dump*.*  | awk 'NF{print $3, $4, $6, $7, $9, $10, $2, $NF} !NF' > hm
+../scripts/lammps2punto.sh data-wall/dump*.*  | awk 'NF{print $3, $4, $6, $7, $9, $10, $2, $NF} !NF' > hm
--- examples/USER/sph/square-to-sphere/run.sh
+++ examples/USER/sph/square-to-sphere/run.sh
@@ -2,7 +2,7 @@
 
 nx=20
 ndim=3
-np=8
+np=2
 dname=data-nx${nx}-ndim${ndim}-np${np}a
 mkdir -p ${dname}
 mpirun -np ${np}  ../../../../src/lmp_linux -in insert.lmp -var ndim ${ndim} -var nx ${nx} -var dname ${dname}
