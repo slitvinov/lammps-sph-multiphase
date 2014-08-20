cec0b8382b8d36a6fa5f6760d3eafc4ccf51eef9
Litvinov Sergey <slitvinov@gmail.com>
2014-08-20 11:15:39 +0200

    Rearange examples
    Former-commit-id: c384adcd4490f5b3afb4ed04e7ef634b9d15bd98 [formerly 913d1a3bb5b85057b91943120c49c902d8dfb331]
    Former-commit-id: 319de54fa6d4abd419aafc3241dc96d71ebc99cd

--- examples/USER/sph/contact-angle/post.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# x, y, fx, fy, colorgradient_peratom
-../scripts/lammps2punto.sh data-wall/dump*.*  | awk 'NF{print $3, $4, $6, $7, $9, $10, $2, $NF} !NF' > hm
--- examples/USER/sph/contact_angle/post.tcl
+++ examples/USER/sph/contact_angle/post.tcl
@@ -9,6 +9,5 @@ user add key q exit
 set sel_solvent [atomselect top "resname 2"]
 $sel_solvent set name droplet
 
-
 set sel_solvent [atomselect top "resname 3"]
 $sel_solvent set name boundary
