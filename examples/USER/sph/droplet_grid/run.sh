6e66ccbad47439c4dddd052d06699399d1b91da7
Litvinov Sergey <slitvinov@gmail.com>
2014-08-25 17:11:54 +0200

    Update capillary wave example
    Former-commit-id: c1e6096c4299943fac0578941a7000c2ca9ef0cd

--- examples/USER/sph/droplet_grid/run.sh
+++ examples/USER/sph/droplet_grid/run.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-nx=40
+# 40, 68, 126
+nx=126
 dname=data-wall-nx${nx}
 
 # case number (1 or 2)
 icase=2
 mkdir -p ${dname}
-../../../../src/lmp_linux -in droplet.lmp -var icase ${icase} -var dname ${dname} -var nx ${nx}
+/scratch/prefix-mpich3.1.2/bin/mpirun -np 8 ../../../../src/lmp_linux -in droplet.lmp -var icase ${icase} -var dname ${dname} -var nx ${nx}
