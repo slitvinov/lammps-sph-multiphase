43c6b9409548513642d3666a2217af03561ba80c
Litvinov Sergey <slitvinov@gmail.com>
2014-08-26 19:51:36 +0200

    Update bubble_growth to match bubble_random
    Former-commit-id: 829ce613cf3f493444507efc987fafa02156b40c

--- examples/USER/sph/bubble_growth/run.sh
+++ examples/USER/sph/bubble_growth/run.sh
@@ -3,10 +3,11 @@
 set -e
 set -u
 
-ndim=2
+ndim=3
+nx=20
 lmp=../../../../src/lmp_linux
 
-dname=data-ndim${ndim}
-~/prefix-mpich/bin/mpirun -n 1 ${lmp} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
+dname=data-ndim${ndim}-nx${nx}
+${lmp} -var nx ${nx} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
     
     
