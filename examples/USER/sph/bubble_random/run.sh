ca41e5e041388426bad4b80f4aa511d7da5c6cbb
Litvinov Sergey <slitvinov@gmail.com>
2014-08-26 18:30:45 +0200

    Restore parameters to the ones in the paper
    Former-commit-id: 3c6a364af2ba8a461b250229f3e029a935f201c7

--- examples/USER/sph/bubble_random/run.sh
+++ examples/USER/sph/bubble_random/run.sh
@@ -4,9 +4,10 @@ set -e
 set -u
 
 ndim=3
+nx=20
 lmp=../../../../src/lmp_linux
 
-dname=data-ndim${ndim}
-/scratch/prefix-mpich3.1.2/bin/mpirun  -n 8 ${lmp} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
+dname=data-ndim${ndim}-nx${nx}
+/scratch/prefix-mpich3.1.2/bin/mpirun  -n 8 ${lmp} -var nx ${nx} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
     
     
