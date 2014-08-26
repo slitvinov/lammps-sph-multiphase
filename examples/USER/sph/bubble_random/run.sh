4064062666a5d3856ff7135c815bc234315d7574
Litvinov Sergey <slitvinov@gmail.com>
2014-08-26 19:35:58 +0200

    Clearn up square_to_sphere example
    Former-commit-id: 46e353342fd992318a7968b5ebfe49c1ecfc5ffd

--- examples/USER/sph/bubble_random/run.sh
+++ examples/USER/sph/bubble_random/run.sh
@@ -8,6 +8,6 @@ nx=40
 lmp=../../../../src/lmp_linux
 
 dname=data-ndim${ndim}-nx${nx}
-/scratch/prefix-mpich3.1.2/bin/mpirun  -n 8 ${lmp} -var nx ${nx} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
+${lmp} -var nx ${nx} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
     
     
