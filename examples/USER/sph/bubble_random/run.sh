d2977aabdd1c2ab35eeb74f619f71cf01c89a4d0
Litvinov Sergey <slitvinov@gmail.com>
2014-08-26 16:02:55 +0200

    Update config files
    Former-commit-id: 530f39c6ff17fbe4e593813a4d8e589b128e7a42

--- examples/USER/sph/bubble_random/run.sh
+++ examples/USER/sph/bubble_random/run.sh
@@ -3,10 +3,10 @@
 set -e
 set -u
 
-ndim=3
+ndim=2
 lmp=../../../../src/lmp_linux
 
 dname=data-ndim${ndim}
-/scratch/prefix-mpich3.1.2/bin/mpirun  -n 8 ${lmp} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
+${lmp} -var ndim ${ndim} -var dname ${dname} -in bubble.lmp
     
     
