7579c1a42a8f5d409a8fb750ea14c801b36348ef
Litvinov Sergey <slitvinov@gmail.com>
2015-03-20 23:50:59 +0100

    Change paramters

--- examples/USER/sph/droplet_grid/run.sh
+++ examples/USER/sph/droplet_grid/run.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # 40, 68, 126
-nx=126
+nx=68
 dname=data-wall-nx${nx}
 
 # case number (1 or 2)
-icase=2
+icase=1
 mkdir -p ${dname}
 ../../../../src/lmp_linux -in droplet.lmp -var icase ${icase} -var dname ${dname} -var nx ${nx}
