c67aa43294230e43626c5debee3ca347c573c06b
Litvinov Sergey <slitvinov@gmail.com>
2015-03-20 23:50:28 +0100

    Update contact_angle example

--- examples/USER/sph/contact_angle/run.sh
+++ examples/USER/sph/contact_angle/run.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-nx=121
+nx=248
 dname=data-wall-nx${nx}
 
 # number of a case
-icase=2
+icase=1
 mkdir -p ${dname}
 lmp=../../../../src/lmp_linux
 ${lmp} -in droplet.lmp -var icase ${icase} -var dname ${dname} -var nx ${nx}
